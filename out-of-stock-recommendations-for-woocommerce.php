7bf3e3f76b5f0fd78573e43ae1e3645702b5ead0
Mike Moore <michael.moore@automattic.com>
2021-03-29 23:21:52 -0400

    Initial commit

---
+++ out-of-stock-recommendations-for-woocommerce.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Plugin Name: Out of Stock Recommendations for WooCommerce
+ * Plugin URI: https://github.com/mdmoore/out-of-stock-recommendatons-for-woocommerce
+ * Description: Recommend a specific product when a product is out of stock
+ * Author: Mike Moore
+ * Author URI: https://github.com/mdmoore
+ * Text Domain: out-of-stock-recommendations-for-woo
+ * Version: 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'Out_Of_Stock_Recommendations_For_WooCommerce' ) ) {
+	/**
+	 * Main class.
+	 *
+	 * @package Out_Of_Stock_Recommendations_For_WooCommerce
+	 * @since   1.0.0
+	 */
+	class Out_Of_Stock_Recommendations_For_WooCommerce {
+
+    }
+}
